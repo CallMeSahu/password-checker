c4934a3383773b305bb450df30263751241aaab9
Siddhartha Sahu <54734745+CallMeSahu@users.noreply.github.com>
2021-08-25 01:07:49 +0530

    feat: password health

--- src/App.js
+++ src/App.js
@@ -1,20 +1,72 @@
+import { render } from "react-dom";
 import "./styles.css";
+import React, { useState } from "react";
 
 export default function App() {
+  var currentScore = 8;
+
+  const [textColor, setTextColor] = useState("red");
+  const [health, sethealth] = useState("");
+  const [sugg1, setsugg1] = useState("");
+  const [sugg2, setsugg2] = useState("");
+  const [sugg3, setsugg3] = useState("");
+  const [sugg4, setsugg4] = useState("");
+
+  function checkNumberOfChar(string) {
+    for (var i = 0; i < string.length; i++) {}
+  }
+
+  function checkUpperLowerChar(string) {
+    for (var i = 0; i < string.length; i++) {}
+  }
+
+  function checkNumericChar(string) {
+    for (var i = 0; i < string.length; i++) {}
+  }
+
+  function checkSpecialChar(string) {
+    for (var i = 0; i < string.length; i++) {}
+  }
+
   function passwordInputHandler(passwordInput) {
-    console.log(passwordInput.target.value);
+    var userInputString = passwordInput.target.value;
+    checkNumberOfChar(userInputString);
+    checkUpperLowerChar(userInputString);
+    checkNumericChar(userInputString);
+    checkSpecialChar(userInputString);
+
+    if (currentScore >= 6) {
+      setTextColor("green");
+      sethealth("Excellent");
+    } else if (currentScore < 6 && currentScore >= 2) {
+      setTextColor("orange");
+      sethealth("Good");
+    } else {
+      setTextColor("red");
+      sethealth("Bad");
+    }
   }
 
   return (
     <div className="App">
-      <h1>Password Checker</h1>
+      <h1>Password Health Checker</h1>
       <p className="label">Check if your password is safe or not!</p>
-      <div style={{ border: "1px lightgreen" }}>
+      <div className="input-area">
         <input
           className="input-box"
           type="password"
           onChange={passwordInputHandler}
         ></input>
+      </div>
+      <div className="output">
+        <p>
+          Password Health: <span style={{ color: textColor }}>{health}</span>
+        </p>
+        <h4>Suggestion</h4>
+        <p>{sugg1}</p>
+        <p>{sugg2}</p>
+        <p>{sugg3}</p>
+        <p>{sugg4}</p>
       </div>
     </div>
   );
